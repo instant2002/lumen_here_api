6d73dc2ea399819100b212ab80448f3005fb9d35
yunsik-chae <yunsik.chae@goodoc.co.kr>
2025-08-10 01:20:37 +0900

    fix: 유저생성 유효성 규칙 수정

--- src/user/domain/value-objects/password.vo.ts
+++ src/user/domain/value-objects/password.vo.ts
@@ -11,12 +11,12 @@ export class PasswordVO {
       throw new Error('비밀번호는 8자리 이상이어야 합니다');
     }
 
-    if (password.length > 20) {
-      throw new Error('비밀번호는 20자리 이하여야 합니다');
+    if (password.length > 15) {
+      throw new Error('비밀번호는 15자리 이하여야 합니다');
     }
 
-    if (!password.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]/)) {
-      throw new Error('비밀번호는 대문자, 소문자, 숫자, 특수문자를 포함해야 합니다');
+    if (!password.match(/^(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]/)) {
+      throw new Error('비밀번호는 특수문자를 포함해야 합니다');
     }
   }
 
--- src/user/presentation/dto/input/create-user.input.ts
+++ src/user/presentation/dto/input/create-user.input.ts
@@ -1,5 +1,5 @@
 import { Field, InputType } from '@nestjs/graphql';
-import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+import { IsEmail, IsNotEmpty } from 'class-validator';
 
 @InputType()
 export class CreateUserInput {
@@ -10,7 +10,6 @@ export class CreateUserInput {
 
   @Field()
   @IsNotEmpty()
-  @MinLength(6)
   password: string;
 
   @Field()
