d59e6b1b17bf1089e6bbc4c714944c432f4aeca3
yunsik-chae <yunsik.chae@goodoc.co.kr>
2025-08-13 13:35:28 +0900

    test: 코드래빗 테스트

--- src/user/presentation/resolvers/user.resolver.ts
+++ src/user/presentation/resolvers/user.resolver.ts
@@ -9,7 +9,7 @@ export class UserResolver {
 
   @Query(() => UserOutput)
   async getUser(@Args('id') id: number): Promise<UserOutput> {
-    const user = await this.userService.findUniqeu(id);
+    const user = await this.userService.findUniqeue(id);
     return new UserOutput(user);
   }
 
